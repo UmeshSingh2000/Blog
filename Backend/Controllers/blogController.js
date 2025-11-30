66eda033e877805d17d013d448d56323c1f85701
Umesh singh <umeshsinghmehta4@gmail.com>
2025-11-30 15:21:54 +0530

    fix: Change default limit for blog retrieval to 0 for unbounded results

--- Backend/Controllers/blogController.js
+++ Backend/Controllers/blogController.js
@@ -357,7 +357,7 @@ const getBlogs = async (req, res) => {
   try {
     // console.log('running')
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 0;
     const skip = (page - 1) * limit;
 
     const totalBlogs = await Blog.countDocuments({ status: "published" });
